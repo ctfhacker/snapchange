a98d9168061443d2f10bbe7776c933e91bef3df4
Cory Duplantis <corydu@amazon.com>
2023-12-20 22:00:48 +0000

    Ensure mutation chain of files is written to disk during RQ

--- src/stats.rs
+++ src/stats.rs
@@ -1892,6 +1892,11 @@ pub fn worker<FUZZER: Fuzzer>(
 
                                 if is_new {
                                     drop(curr_metadata);
+
+                                    // Ensure this input has been written
+                                    save_input_in_project(&input, project_dir, "current_corpus")
+                                        .unwrap();
+
                                     total_corpus.insert(input);
                                 }
                             }
