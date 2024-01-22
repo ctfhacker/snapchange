cd8e54322ad9012f27eea872ba81c8a6bfe25943
Cory Duplantis <cld251@gmail.com>
2024-01-22 15:53:18 -0600

    Add initial flake.nix for development shell for snapchange

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Snapchange development environment";
+
+  # Flake inputs
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+
+    # Rust overlay for Rust nightly
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Flake utils for eachDefaultSystem
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem(system: 
+      let
+        # Use the rust overlay to use Rust nightly
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+              packages = [
+                # Python
+                (python311.withPackages (ps: with ps; [
+                  virtualenv 
+                  pip 
+                  ipython
+                  ipdb
+                ]))
+
+                # Rust nightly
+                rust-bin.nightly.latest.default
+              ];
+
+              shellHook = ''
+                # Install binaryninja into a local virtual envirionment
+                # If the virtual environment has not been created, create it
+                if [ ! -d "./.venv" ]; then
+                  BINJA=$(readlink $(which binaryninja))
+                  BINJA_PATH=$(dirname $BINJA)/..
+
+                  python3 -m venv ./.venv;
+                  source ./.venv/bin/activate;
+
+                  python3 $BINJA_PATH/opt/scripts/install_api.py --install-on-pyenv
+                else
+                  # If the virtual environment exists, use it
+                  source ./.venv/bin/activate
+                fi
+              '';
+            };
+          });
+}
