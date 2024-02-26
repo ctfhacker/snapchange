26507427463f929061559f7f06428109d3057694
Cory Duplantis <cld251@gmail.com>
2024-02-26 17:38:12 -0600

    Init docker build from flake

--- flake.nix
+++ flake.nix
@@ -75,5 +75,34 @@
                 fi
               '';
             };
+
+            packages.snapshotter = dockerTools.buildImage {
+              name = "Snapchange_Snapshotter";
+              tag = "latest";
+
+              /*
+              # Only for testing
+              fromImage = pkgs.dockerTools.pullImage {
+                imageName = "ubuntu";
+                imageDigest = "sha256:83f0c2a8d6f266d687d55b5cb1cb2201148eb7ac449e4202d9646b9083f1cee0";
+                sha256 = "sha256-5y6ToMw1UGaLafjaN69YabkjyCX61FT3QxU4mtmXMP0=";
+                finalImageName = "ubuntu";
+                finalImageTag = "latest";
+                os = "linux";
+                arch = "x86_64";
+              };
+              */
+
+              copyToRoot = buildEnv {
+                name = "image-root";
+                paths = [
+                  customQemu
+                  bashInteractive
+                ];
+                pathsToLink = [
+                  "/bin"
+                ];
+              };
+            };
           });
 }
