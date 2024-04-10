2b942843c4bfc4fe5680556df2c33ab1d64cc538
Cory Duplantis <cld251@gmail.com>
2024-04-10 12:45:00 -0500

    Remove unneeded files from ex02_no_patch

--- examples/02_libtiff_no_patch/reset.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-rm -rf snapshot
-rm -rf target
-rm fuzzer.log
-
-docker rmi --force snapchange_example2_no_patch:fuzzer
-docker rmi --force snapchange_example2_no_patch:snapshot
-docker rmi --force snapchange_example2_no_patch:target
